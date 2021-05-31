82895b65bc29810bbc2c53473fdd4df2221be3ce
Nazariy Vysokinskyi <n.vysokinskyi@gmail.com>
2021-05-31 19:30:34 +0300

    Initial Commit

---
+++ PDF Expert/PDF_ExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ExpertApp.swift
+//  PDF Expert
+//
+//  Created by Nazariy Vysokinskyi on 31.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
